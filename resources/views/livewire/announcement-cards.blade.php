b3e9b5e41e1080c002bccc238fdc4b1707aaf31d
Nicolò D'alessandro <nicolodalessandro97@gmail.com>
2022-06-21 15:04:09 +0200

    graphics adjustments on announces cards

--- resources/views/livewire/announcement-cards.blade.php
+++ resources/views/livewire/announcement-cards.blade.php
@@ -3,11 +3,13 @@
     @foreach ($announces as $announce)
     <div class="col-12 col-lg-4 text-center">
       <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="https://source.unsplash.com/200x200" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">{{$announce->title}}</h5>
             <p class="card-text">{{$announce->description}}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text">{{$announce->category->name}}</p>
+            <p class="card-text">{{$announce->price}} €</p>
+            <a href="#" class="btn btn-primary">Dettagli</a>
           </div>
         </div>
     </div>
