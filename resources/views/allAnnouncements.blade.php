ebbee26fdfacc213ecfe32efff160bf6629d3d2b
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-06-21 18:43:30 +0200

    added filters structure to allAnnouncements view

--- resources/views/allAnnouncements.blade.php
+++ resources/views/allAnnouncements.blade.php
@@ -4,24 +4,26 @@
     @if (session('message'))
         <div class="alert alert-success">
             {{session('message')}}
-        </div>        
+        </div>
     @endif
     
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-4">
-                <div class="form-floating">
-                    <select class="form-select" id="floatingSelect">
-                        <option selected>Tutte le categorie</option>
+            <div class="col-3 mt-5">
+                <div class="form-floating my-3">
+                    <input type="text" class="form-control" id="searchByTextInput" placeholder="Cerca per parola">
+                    <label for="searchByTextInput">Cerca per parola</label>
+                </div>
+                <div class="form-floating my-3">
+                    <select id="searchByCategoryInput" class="form-select" id="floatingSelect">
+                        <option value="all" selected>Tutte le categorie</option>
                         @foreach ($categories as $category)
                             <option value="{{$category->id}}">{{$category->name}}</option>
                         @endforeach
                     </select>
                     <label for="floatingSelect">Scegli la categoria</label>
-                  </div>
-            </div>
-            <div class="col-12 col-md-4">
-                <div class="form-floating">
+                </div>
+                <div class="form-floating my-3">
                     <select class="form-select" id="orderSelect">
                         <option value="recentToOlder" selected>Dal più recente al più vecchio</option>
                         <option value="olderToRecent">Dal più vecchio al più recente</option>
@@ -31,13 +33,26 @@
                         <option value="zToA">Dalla Z alla A</option>
                     </select>
                     <label for="orderSelect">Ordina annunci</label>
-                  </div>
+                </div>
+                <div id="priceSearchRow" class="row mx-auto">
+                    <div id="minPriceInputDiv" class="form-floating my-2 pe-1 ps-0 w-50">
+                        <input type="number" class="form-control" id="minInput" placeholder="Min price">
+                        <label id="priceLabels" for="minInput">Min price</label>
+                    </div>
+                    <div id="maxPriceInputDiv" class="form-floating my-2 ps-1 pe-0 w-50">
+                        <input type="number" class="form-control" id="maxInput" placeholder="Max price">
+                        <label id="priceLabels" for="maxInput">Max price</label>
+                    </div>
+                </div>
+                <div id="searchFilterBtnCol" class="col-4 m-auto text-center">
+                    <button id="searchFilterBtn" class="">Cerca</button>
+                </div>
+            </div>
+            <div class="col-9 mt-5">
+                <livewire:announcement-cards/>
             </div>
         </div>
     </div>
 
-    <div class="container mt-5">
-        <livewire:announcement-cards/>
-    </div>
     
 </x-layout>
