bb3f6f2ac2d2b3e05161a8269ffe9b2e474743a4
Nicola Salzano <nicolasalzano2913@gmail.com>
2022-06-26 11:12:36 +0200

    modified navbar ui

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,50 +1,59 @@
- <div class="container-fluid p-0 m-0">
-  <header class="d-flex  align-items-center justify-content-center justify-content-md-between mb-4 ps-5 fixed-top presto-nav ">
-    <a href="{{route('welcome')}}" class=" title-nav panna d-flex align-items-center text-decoration-none">
-      Presto
-    </a>
+<nav class="navbar navbar-expand-md presto-nav fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand title-nav panna text-decoration-none" href="{{route('welcome')}}">Presto</a>
+    <button class="navbar-toggler orange" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    </button>
 
-    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-      <li><a href="{{route('welcome')}}" class="nav-link px-4 fs-5 lato">Home</a></li>
-      <li><a href="{{route('allAnnouncements')}}" class="nav-link px-4 lato fs-5">Annunci</a></li>
-      @guest    
-      <li class="nav-item">
-          <a class="nav-link fs-5 px-4" href="{{route('register')}}">Registrati</a>
+    <div class="collapse navbar-collapse" id="navbarsExample04">
+      <ul class="navbar-nav ms-auto dropBg mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link active fs-6 px-4"  href="{{route('welcome')}}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fs-6 px-4" href="{{route('allAnnouncements')}}">Annunci</a>
+        </li>
+        @guest
+        <li class="nav-item">
+          <a class="nav-link fs-6 px-4" href="{{route('register')}}">Registrati</a>
       </li>
       <li class="nav-item">
-          <a class="nav-link fs-5 px-4" href="{{route('login')}}">Log-in</a>
+          <a class="nav-link fs-6 px-4" href="{{route('login')}}">Log-in</a>
       </li>
       @else
-      <li class="nav-item">
-          <a class="nav-link fs-5" href="{{route('createAnnouncement')}}">Nuovo annuncio</a>
-      </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle fs-5 px-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Ciao {{Auth::user()->name}}
+        <a class="nav-link dropdown-toggle fs-6 px-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Ciao {{Auth::user()->name}}
         </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Profilo</a></li>
-            <li><a class="dropdown-item" href="#">I miei annunci</a></li>
-            
-            @if (Auth::user()->is_revisor)     {{--  Se l'utente è un revisore  --}}                      
-            <li>
-              <a class="dropdown-item position-relative" href="{{route('revisor.index')}}">Zona revisore
-            <span class="position-absolute top-50 badge-class translate-middle badge rounded-pill bg-danger">
-                {{App\Models\Announcement::toBeRevisionedCount()}}
-                <span class="visually-hidden">unread messages</span>
-            </span>
-            </a>
-            </li>
-            <li><hr class="dropdown-divider"></li>
-            @endif
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('frm-logout').submit();">Logout</a>      
-            <form id="frm-logout" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-            </form>
+        <ul class="dropdown-menu dropBg" aria-labelledby="dropdown04">
+          <li>
+            <a class="dropdown-item" href="#">Profilo</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">I tuoi annunci</a>
+          </li>
+          @if (Auth::user()->is_revisor)     {{--  Se l'utente è un revisore  --}}                      
+          <li>
+            <a class="dropdown-item position-relative" href="{{route('revisor.index')}}">Zona revisore
+          <span class="position-absolute top-50 badge-class translate-middle badge rounded-pill bg-danger">
+              {{App\Models\Announcement::toBeRevisionedCount()}}
+              <span class="visually-hidden">unread messages</span>
+          </span>
+          </a>
+          </li>
+          <li><hr class="dropdown-divider"></li>
+          @endif
+          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('frm-logout').submit();">Logout</a>      
+          <form id="frm-logout" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+          </form>
         </ul>
-    </li>
-    @endguest
-    </ul>
-  </header>
-</div>
-
+      </li>
+      <li class="nav-item">
+          <a class="nav-link fs-6 px-4" href="{{route('createAnnouncement')}}">Nuovo annuncio</a>
+      </li>
+        @endguest
+      </ul>
+    </div>
+  </div>
+</nav>
