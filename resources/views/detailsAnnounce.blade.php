2020280865dec81884f33b6844dd8fa30bcc3c71
Nicolò D'alessandro <nicolodalessandro97@gmail.com>
2022-06-26 13:13:30 +0200

    refactoring ui deatails announce

--- resources/views/detailsAnnounce.blade.php
+++ resources/views/detailsAnnounce.blade.php
@@ -5,102 +5,82 @@
     Dettagli {{$announce->title}}
   </x-slot>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center">{{$announce->title}}</h1>
+   
+<div class="container mt-5 min-vh-100">
+  <div class="row">
+    <div class="col-12 col-md-6">
+      <!-- Carousel wrapper -->
+     <div id="carouselMDExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+       <!-- Slides -->
+
+       <div class="carousel-inner mb-5 shadow-1-strong rounded-3">
+          @foreach ($announce->images as $image)
+            <div class="carousel-item @if($loop->first)active @endif">
+              <img src="{{!$announce->images()->get()->isEmpty() ? $image->getUrl(600,450) : 'https://source.unsplash.com/230x200'}}" alt="Immagine bella" class="d-block w-100">
             </div>
-        </div>
+          @endforeach
+       </div>
+       <!-- Slides -->
+     
+       <!-- Controls -->
+       <button class="carousel-control-prev" type="button" data-mdb-target="#carouselMDExample"
+         data-mdb-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Previous</span>
+       </button>
+       <button class="carousel-control-next" type="button" data-mdb-target="#carouselMDExample"
+         data-mdb-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Next</span>
+       </button>
+       <!-- Controls -->
+     
+       <!-- Thumbnails -->
+       <div class="carousel-indicators" style="margin-bottom: -20px;">
+          @foreach ($announce->images as $image)
+          <button type="button" data-mdb-target="#carouselMDExample" data-mdb-slide-to="{{$loop->index}}" class="@if($loop->first)active @endif"
+            aria-current="true" aria-label="Slide 1" style="width: 100px;">
+            <img class="d-block w-100 shadow-1-strong rounded"
+              src="{{!$announce->images()->get()->isEmpty() ? $image->getUrl(600,450) : 'https://source.unsplash.com/230x200'}}"" class="img-fluid" />
+          </button>
+          @endforeach
+         
+       </div>
+       <!-- Thumbnails -->
+     </div>
     </div>
-    
 
-   {{-- <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="https://source.unsplash.com/600x200" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="https://source.unsplash.com/600x200" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="https://source.unsplash.com/600x200" class="d-block w-100" alt="...">
-                  </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-        </div>
-    </div> --}}
-
-    <div class="container">
-      <div class="row">
-        <div class="col-6">
-          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-            {{-- <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            </div> --}}
-            <div class="carousel-inner">
-
-              @foreach ($announce->images as $image)
-
-                <div class="carousel-item @if($loop->first)active @endif">
-                    <img src="{{!$announce->images()->get()->isEmpty() ? $image->getUrl(600,450) : 'https://source.unsplash.com/230x200'}}" alt="Immagine bella" class="bellaFoto img-fluid">
-                </div>
-              @endforeach
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+    <div class="col-12 col-md-6">
+      <div class="row mt-5">
+          <div class="col-12">
+              <p class="titleDetails">{{$announce->title}}</p>
           </div>
-        </div>
+          <div class="col-12">
+              <p class="fs-5 lato">{{$announce->description}}</p>
+          </div>
+          <div class="col-12 lato fs-5">
+              <p>Prezzo : {{$announce->price}} €</p>
+          </div>
+          <div class="col-12 lato fs-5">
+              <a href="{{route('categoryShow',$announce->category)}}" class="card-text">{{$announce->category->name}}</a>
+          </div>
+          <div class="col-12 mt-3">
+              <p class="lato fs-6 mt-5 created">Creato da {{$announce->user->name}} il {{$announce->created_at->format('d/m/Y')}}</p>
+          </div>
       </div>
     </div>
+
+  </div>
+
+</div>
+
+
+
+
+
+
+
     
-
-
-
-
-
-
-
-    <div class="row mt-5">
-        <div class="col-12">
-            <p>Titolo : {{$announce->title}}</p>
-        </div>
-        <div class="col-12">
-            <p>Decrizione : {{$announce->description}}</p>
-        </div>
-        <div class="col-12">
-            <p>Prezzo : {{$announce->price}}</p>
-        </div>
-        <div class="col-12">
-            <a href="{{route('categoryShow',$announce->category)}}" class="card-text">{{$announce->category->name}}</a>
-        </div>
-        <div class="col-12 mt-3">
-            <p class="card-text">Creato il : {{$announce->created_at->format('d/m/Y')}}</p>
-        </div>
-    </div>
-   </div>
 
    <x-footerGeneral></x-footergeneral>
 </x-layout>
