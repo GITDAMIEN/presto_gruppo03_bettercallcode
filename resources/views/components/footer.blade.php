3a61b3e1e0ec38e808a0c7c1ffae7965920253db
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-06-28 09:30:35 +0200

    responsiveness in footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,23 @@
 <div class="container-fluid presto-foo mt-5 mb-0 ">
-    <footer class="pt-5 mt-5">
+    <footer class="pt-4 mt-5">
       <div class="row">
-        <div class="col-2">
+                
+        <div class="col-6 col-md-2 pt-2">
+          <h5 id="footerH5" class="px-3">Presto.it</h5>
+          <ul class="nav flex-column px-3">
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 ">Chi siamo</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="{{route('createAnnouncement')}}" class="nav-link p-0 ">Vendi</a>
+            </li>
+            <li class="nav-item mb-2">
+              <a href="#" class="nav-link p-0 ">Lavora con noi</a>
+          </li>
+        </ul>
+      </div>
+
+        <div class="col-6 col-md-2 pt-2">
           <h5 id="footerH5" class="px-3">Supporto clienti</h5>
           <ul class="nav flex-column px-3">
             <li class="nav-item mb-2">
@@ -19,7 +35,7 @@
           </ul>
         </div>
         
-        <div class="col-2">
+        <div class="col-6 col-md-2 pt-2">
           <h5 id="footerH5" class="px-3">Termini d'uso</h5>
           <ul class="nav flex-column px-3">
             <li class="nav-item mb-2">
@@ -36,47 +52,35 @@
             </li>
           </ul>
         </div>
-        
-        <div class="col-2">
-          <h5 id="footerH5">Presto.it</h5>
-          <ul class="nav flex-column">
-            <li class="nav-item mb-2">
-              <a href="#" class="nav-link p-0 ">Chi siamo</a>
-            </li>
-            <li class="nav-item mb-2">
-              <a href="{{route('createAnnouncement')}}" class="nav-link p-0 ">Vendi</a>
-            </li>
-            <li class="nav-item mb-2">
-              <a href="#" class="nav-link p-0 ">Lavora con noi</a>
-          </li>
-        </ul>
-      </div>
       
-      <div class="col-4 offset-1">
-        <form>
+      <div class="col-12 col-md-4 newsletterDiv px-2 pt-4">
+        <form class="px-3">
           <h5 id="footerH5">Newsletter</h5>
-          <div class="d-flex w-100 gap-2">
+          <div class="d-flex gap-2">
             <label for="newsletter1" class="visually-hidden">Indirizzo email</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-            <button class="btn papayaBg panna" type="button"> <i class="fa-solid fa-paper-plane"></i> </button>
+            <input id="newsletter1" type="text" class="col-10 form-control" placeholder="Email address">
+            <button class="btn papayaBg panna col-2" type="button"> <i class="fa-solid fa-paper-plane"></i> </button>
           </div>
         </form>
       </div>
     </div>
     
-    <div class="d-flex justify-content-between pt-4 mt-4 border-top">
-      <p class="panna">© 2022 Presto.it</p>
-      <ul class="list-unstyled d-flex fs-4">
-        <li class="me-3 px-2">
-          <a class="panna p-font" href="#"><i class="fa-brands fa-instagram"></i></a>
-        </li>
-        <li class="me-3 px-2">
-          <a class="panna p-font" href="#"><i class="fa-brands fa-twitter"></i></a>
-        </li>
-        <li class="me-3 px-2">
-          <a class="panna p-font" href="#"><i class="fa-brands fa-tiktok"></i></a>
-        </li>
-      </ul>
+    <div class="d-flex justify-content-between ps-2 py-3 mt-4 border-top">
+      <span id="footerPrestoLogo" class="col-12 col-md-3 panna mb-0">© 2022 Presto.it</span>
+      <div class="col-12 col-md-6 text-end">
+        <ul class="list-unstyled d-flex fs-4 mb-0 panna d-flex justify-content-end">
+          <li class="mb-0 me-3">Seguici sui social!</li>
+          <li class="me-3 px-2">
+            <a class="panna p-font" href="#"><i class="fa-brands fa-instagram"></i></a>
+          </li>
+          <li class="me-3 px-2">
+            <a class="panna p-font" href="#"><i class="fa-brands fa-twitter"></i></a>
+          </li>
+          <li class="me-3 px-2">
+            <a class="panna p-font" href="#"><i class="fa-brands fa-tiktok"></i></a>
+          </li>
+        </ul>
+      </div>
     </div>
   </footer>
 </div>
