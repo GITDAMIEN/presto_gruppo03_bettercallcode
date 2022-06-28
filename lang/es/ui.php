d70f55ff719947b470a84acbf3fe7fdd0b43a1fa
Nicola Salzano <nicolasalzano2913@gmail.com>
2022-06-28 03:04:21 +0200

    add multilanguage es

---
+++ lang/es/ui.php
@@ -0,0 +1,102 @@
+<?php
+return [
+    //REGISTER
+    'reg'       => 'Registrarse',
+    'reg-title' => 'Registrarse en Presto.it',
+    'reg-mail'  => 'Inserta el tuyo email',
+    'reg-user'  => 'Inserta el tuyo username',
+    'reg-pass'  => 'Inserta el tuyo password',
+    'reg-conf'  => 'Confirma el tuyo password',
+    
+    //LOGIN
+      'log-rem' => 'Recuérdame',
+      'log-btn' => 'Registrarse',
+      'log-title' => 'Hacer el login en Presto.it',
+
+    
+    //NUOVO ANNUNCIO
+      'new-ann' => 'Nuevo anuncio',
+      'add-ann' => 'Publicar anuncio',
+      'add-title' => 'Tìtulo',
+      'add-desc' => 'Describa lo que está vendiendo',
+      'cate' => 'Categoría',
+      'price' => 'Precio',
+      'add-btn' => 'Publicar',
+      'add-dele' => 'Cancelar',
+
+      //LAST ANNOUNCEMENT
+      'details' => 'Detalles',
+      'create'  => 'Creado por',
+      'at' => 'la',
+      'add-first-ann' => 'Insertas el primer anuncio',
+
+      //CATEGORY SHOW
+      'cate-show' => 'Estás viendo la categoría',
+      'createAt'  => 'Creado en',
+
+      //ALL ANNOUNCEMENT
+      'all-ann' => 'Todos los anuncios',
+      'all-ann-g' => 'TODOS LOS ANUNCIOS',
+
+      //INDEX
+      'rev-zone' => 'Área de revisores',
+      'ann-rev'  => 'Anuncio a revisar:',
+      'ann-rev-no'  => 'No hay anuncios para revisar',
+      'description'  => 'Descripción',
+      'uploadBy'  => 'Cargado da',
+      'acceptAnn'  => 'Aceptar anuncio',
+      'acc-ann'  => 'Aceptar anuncio',
+      'rif-ann'  => 'Rechazar anuncio',
+      'last-ann-acc'  => 'Los últimos anuncios aceptados',
+      'rif-ann-btn'  => 'Cancelar revisión y rechazar anuncio',
+      'rif-ann-no'  => 'No se han aceptado anuncios hasta ahora',
+      'last-ann-rif'  => 'Los últimos anuncios rechazados',
+      'acc-ann-no'  => 'Ningún anuncio rechazado hasta ahora',
+      'ann-rev-acc-ann' => 'Cancelar revisión y aceptar anuncio',
+
+      //REVISOR FORM
+      'add-req' => 'Complete la solicitud',
+      'obb-camp' => 'Este campo es obligatorio',
+      'add-email-corr' => 'Por favor introduzca un email valido',
+      'mess' => 'Mensaje',
+      'add-CV' => 'Sube tu CV',
+      'send-req' => 'Enviar la solicitud',
+
+      //ANNOUNCEMENT-CARD
+      'search-word' => 'Buscar por palabra',
+      'all-cate' => 'Todas las categorías',
+      'search-cate' => 'Elige la categoría',
+      'newToOld'=> 'De lo más nuevo a lo más antiguo',
+      'oldToNew' => 'Del más antiguo al más nuevo',
+      'cheapToHigh' => 'Del más barato al más caro',
+      'highToCheap' => 'Del más caro al más barato',
+      'AtoZ' => 'De la A a la Z',
+      'ZtoA' => 'Da la Z a la A',
+      'ord-ann' => 'Ordenar anuncios',
+
+      //NAVBAR
+
+      'ann'=> 'Anuncios',
+      'ciao' =>'Hola',
+      'profilo' =>'Perfil',
+      'your-ann' =>'Tus anuncios',
+      'bec-rev' => 'Conviértete en revisor',
+
+      //WELCOME
+      'home' => 'Homepage',
+      'reg-bec-rev' => 'Regístrese y conviértase en revisor',
+      'home-title' => 'Compra y vende con facilidad... Estés donde estés!',
+      'buy'=> 'Comprar',
+      'sell'=> 'Vender',
+      'last-ann'=> 'LOS ÚLTIMOS ANUNCIOS AÑADIDOS',
+      'our-cate'=> 'NUESTRAS CATEGORIAS',
+      'start-earn'=> 'Empezar a ganar',
+      'home-mess'=> 'Vende productos que ya no uses lo antes posible',
+      'bec-rev-presto'=> 'Conviértete en revisor de Presto.it!',
+      'work-wu'=> 'Trabaja con nosotros!',
+      'bec-rev-g'=> 'CONVIÉRTETE EN REVISOR',
+
+];
+
+
+
